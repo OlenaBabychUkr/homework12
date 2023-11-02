f384f2ad22834ee4025fe274e9062497fd859d54
OlenaBabychUkr <148278289+OlenaBabychUkr@users.noreply.github.com>
2023-11-02 20:06:33 +0200

    Add files via upload

--- Babych-12-2.py
+++ Babych-12-2.py
@@ -2,20 +2,29 @@
 #Write a decorator that wraps a function in a try-except block 
 #and prints an error if any type of error has happened. Example:
 
+exists_dict = {'key': 'bar'}
+
 def catch_errors(func):
     def wrapper(*args, **kwargs):
         try:
             func(*args, **kwargs)
-        except Exception as exp:
-            print(exp, exp.__traceback__)
+        except KeyError as error1:
+            print(error1)
+        except ValueError as error2:
+            print(error2)
+        except TypeError as error3:
+            print(error3)
+        except Exception:
+            print('another error')
     return wrapper
 
 @catch_errors
-def some_function_with_risky_operation(data):
-    if 'key' in data:
-        print(data['key'])
+def some_function_with_risky_operation(data: dict):
+    k = list(data.keys())[0]
+    if k in exists_dict:
+        print(exists_dict.get(k))
     else:
-        print('The data dictionary does not contain a `key` key.')
+        print(f'KeyError no such key as {k}')
 
 
 some_function_with_risky_operation({'foo': 'bar'})
